153ee79b33264ca772648595d9a445d866eb91f2
Zufar Muslimov <zufick@gmail.com>
2024-02-22 22:00:44 +0500

    removed stuff

--- frontend/src/components/ConnectionWindow.vue
+++ frontend/src/components/ConnectionWindow.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const enteredRoomId = ref("");
+    
+function joinGame() {
+    alert("join")
+}
 </script>
 
 <template>
@@ -7,14 +13,9 @@
         <p>Join room</p>
         <div class="flex">
             <input
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="text" placeholder="Room id">
-            <button
-            class="font-semibold hover:bg-black hover:text-white hover:ring hover:ring-white transition duration-300 inline-flex items-center justify-center rounded-md text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black h-10 px-4 py-2"
-            >Join</button>
+            <button @click="joinGame">Join</button>
         </div>
     </div>
-    <button 
-    class="font-semibold hover:bg-black hover:text-white hover:ring hover:ring-white transition duration-300 inline-flex items-center justify-center rounded-md text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black h-10 px-4 py-2"
-    >Create room</button>
+    <button>Create room</button>
 </template>
