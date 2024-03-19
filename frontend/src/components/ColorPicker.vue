2b17c6f55e568369f01ed349c8d6bf3f69c858d7
Zufar Muslimov <zufick@gmail.com>
2024-03-19 11:44:48 +0500

    color picker

---
+++ frontend/src/components/ColorPicker.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+const model = defineModel({default: "#ffffff"})
+
+</script>
+
+<template>
+    <input v-model="model" value="#ffffff" type="color">
+</template>
+
+<style scoped>
+input[type="color"] {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 44px;
+  background: #fff;
+  border-width: 2px;
+  border-color: rgb(68 64 60);
+  border-radius: .375rem;
+  overflow: hidden;
+  outline: none;
+  cursor: pointer;
+}
+
+/* -webkit */
+input[type="color"]::-webkit-color-swatch-wrapper {
+  padding: 6px;
+  background-color: #1c1917;
+}
+
+input[type="color"]::-webkit-color-swatch {
+  border: 2px solid white;  
+  border-radius: .2rem;
+}
+
+
+/* firefox */
+input[type=color]::-moz-focus-inner {
+  border: none;
+  padding: 4px;
+  background-color: #1c1917;
+  border-radius: .1rem;
+}
+
+input[type=color]::-moz-color-swatch {
+  border: none;
+  border-radius: .1rem;
+  height: 140px;
+}
+</style>
--- frontend/src/components/DrawingCanvas.vue
+++ frontend/src/components/DrawingCanvas.vue
@@ -11,6 +11,7 @@ import DrawingCanvasLayer from "./DrawingCanvasLayer.vue";
 import DrawingBrushPreview from "./DrawingBrushPreview.vue";
 import DrawingCanvasClear from './DrawingCanvasClear.vue'
 import DrawingCanvasBrushSize from "./DrawingCanvasBrushSize.vue";
+import ColorPicker from "./ColorPicker.vue";
 
 const wsStore = useWebsocketStore();
 const drawingCanvas = ref<InstanceType<typeof HTMLCanvasElement>>();
@@ -272,7 +273,7 @@ provide('virtualCanvas', virtualCanvas)
             <DrawingBrushPreview :brush="virtualCanvas.brush.value"/>
         </div>
         <div class="flex flex-col items-center gap-4">
-            <input v-model="virtualCanvas.paintBrush.value" value="#ffffff" type="color">
+            <ColorPicker v-model="virtualCanvas.paintBrush.value"/>
             <button @click="virtualCanvas.selectPaintMode()" :class="{'selected-btn': virtualCanvas.brush.value.color == virtualCanvas.paintBrush.value}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
